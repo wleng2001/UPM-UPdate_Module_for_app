14c39d257f153f4a8e7f5bc08f801262f67f6634
wleng2001 <kamilgrodzki5@gmail.com>
2022-10-20 23:00:14 +0300

    add self update

--- UPDATE_MODULE.py
+++ UPDATE_MODULE.py
@@ -1,3 +1,4 @@
+#You can read mor on github website: https://github.com/wleng2001/UPM-UPdate_Module_for_app
 #check and install update
 #import-------------------------
 from datetime import datetime
@@ -5,6 +6,13 @@ from github import Github
 #from git import Repo
 import git
 from os import getcwd
+import requests
+#variables----------------------
+last_update="2022-10-20"
+repo_name="UPM-UPdate_Module_for_app"
+url="https://raw.rawgit.net/wleng2001/UPM-UPdate_Module_for_app/43c899dae7b557b6885f7ded46aff353740ad66d/UPDATE_MODULE.py"
+#new_version_name="UPDATE_MODULE_"+str(datetime.now().date())+".py"
+new_version_name="UPDATE_MODULE.py"
 #update check-------------------
 def update_check(last_update, repo_name):
     last_date=datetime.fromisoformat(last_update)
@@ -20,11 +28,36 @@ def update_check(last_update, repo_name):
     except:
         return None
 #download update
-def download_update(url, path):
+def download_update_repo(url, path):
     try:
-        Repo.clone_from(url, path)
         git.Git(path).clone(url)
+        return True
+    except:        
+        return False
+
+def download_update_file(url, path, file_name): #url must be in raw format for example from rawgit.net website
+    try:
+        file=requests.get(url)
+        file=file.content
+        open(file_name,'wb').write(file)
         return True
     except:
         return False
+    
+def update_self():
+    update_available=update_check(last_update, repo_name)
+    if update_available==None:
+        print("You don't have internet connection.")
+    elif update_available==True:
+        print("Update of update module is available!")
+        ask=input("Do you want download it now (Y/n): ")
+        ask=ask.upper()
+        if_download=None
+        if ask=="Y":
+            if_download=download_update_file(url,getcwd(), new_version_name)
+        if if_download==False:
+            print("Lost connection file wasn't downloaded")
+    else:
+        print("App is current")
+
 
--- example.py
+++ example.py
@@ -1,6 +1,9 @@
 #example for UPM
 import UPDATE_MODULE
 from os import getcwd
+
+UPDATE_MODULE.update_self()
+
 update_available=UPDATE_MODULE.update_check("2022-10-13", "PNG-Prime_Number_Generator")
 if update_available==None:
     print("You don't have internet connection.")
@@ -9,7 +12,7 @@ elif update_available==True:
     ask=input("Do you want download it now (Y/n): ")
     ask=ask.upper()
     if ask=="Y":
-        if_download=UPDATE_MODULE.download_update("https://github.com/wleng2001/PNG-Prime_Number_Generator", getcwd())
+        if_download=UPDATE_MODULE.download_update_repo("https://github.com/wleng2001/PNG-Prime_Number_Generator", getcwd())
         if if_download==False:
             print("Lost connection file wasn't downloaded")
 else:
