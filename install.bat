e8c2edfbc3721e586bd93b667ebf56dda6700add
wleng2001 <kamilgrodzki5@gmail.com>
2023-12-31 20:49:42 +0100

    repair install.bat file

--- install.bat
+++ install.bat
@@ -4,10 +4,10 @@ echo Installing pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 echo Installing python library
-pip install gitdb
-pip install pygithub
-pip install gitpython
-pip install typing_extensions
+pip3 install gitdb
+pip3 install pygithub
+pip3 install gitpython
+pip3 install typing_extensions
 echo if you don't install python you can do it here: https://www.python.org/downloads/
 echo if you don't install git you can do it here: https://gitforwindows.org/
 pause
